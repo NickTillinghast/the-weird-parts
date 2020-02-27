a8f021ea881e7e1bc9d38a1808575e9314bb3957
Nick Tillinghast <nickt2@cox.net>
2020-02-27 11:59:38 -0700

    1

--- app.js
+++ app.js
@@ -7,18 +7,35 @@
 // console.log(a);
 
 
-function b() {
-    var myVar;
-    console.log(myVar)
-}
 
-function a() {
-    var myVar = 2;
-    console.log(myVar);
-    b()
-}
+// function a() {
+//     function b() {
+// var myVar;
+// console.log(myVar)
+// }
+// console.log(myVar);
+//     var myVar = 2;
+//     b()
+// }
 
-var myVar = 1;
-console.log(myVar);
-a();
-console.log(myVar);
+// var myVar = 1;
+// console.log(myVar);
+// a();
+// console.log(myVar);
+
+// ========================
+
+// function waitThreeSeconds() {
+//     var ms = 3000 + new Date().getTime();
+//     while (new Date() < ms) { }
+//     console.log('finished functioin');
+// }
+
+// function clickHandler() {
+//     console.log('click event!')
+// }
+// // listen for the click event
+// document.addEventListener('click', clickHandler);
+
+// waitThreeSeconds();
+// console.log('finished execution');
