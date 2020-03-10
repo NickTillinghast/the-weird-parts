12bc983d285efedbb10f410b804785e6687b2b7e
Nick Tillinghast <nickt2@cox.net>
2020-03-09 23:49:24 -0700

    monday

--- app.js
+++ app.js
@@ -1,66 +1,66 @@
-// var a = "hello world";
+var a = "hello world";
 
-// function b() {
-//     console.log("called B!");
-// };
-// b();
-// console.log(a);
+function b() {
+    console.log("called B!");
+};
+b();
+console.log(a);
 
 
 
-// function a() {
-//     function b() {
-// var myVar;
-// console.log(myVar)
-// }
-// console.log(myVar);
-//     var myVar = 2;
-//     b()
-// }
+function a() {
+    function b() {
+        var myVar;
+        console.log(myVar)
+    }
+    console.log(myVar);
+    var myVar = 2;
+    b()
+}
 
-// var myVar = 1;
-// console.log(myVar);
-// a();
-// console.log(myVar);
+var myVar = 1;
+console.log(myVar);
+a();
+console.log(myVar);
 
-// ========================
+========================
 
-// function waitThreeSeconds() {
-//     var ms = 3000 + new Date().getTime();
-//     while (new Date() < ms) { }
-//     console.log('finished functioin');
-// }
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms) { }
+    console.log('finished functioin');
+}
 
-// function clickHandler() {
-//     console.log('click event!')
-// }
-// // listen for the click event
-// document.addEventListener('click', clickHandler);
+function clickHandler() {
+    console.log('click event!')
+}
+// listen for the click event
+document.addEventListener('click', clickHandler);
 
-// waitThreeSeconds();
-// console.log('finished execution');
+waitThreeSeconds();
+console.log('finished execution');
 
-//=========== because its a function it will be hoisted =========
-// greet();
+=========== because its a function it will be hoisted =========
+greet();
 
-// function greet() {
-//     console.log('hi');
-// }
+function greet() {
+    console.log('hi');
+}
 
-//======== variable will not be hoisted =========
-// var anonymousGreet = function () {
-//     console.log('hi');
-// }
+======== variable will not be hoisted =========
+var anonymousGreet = function () {
+    console.log('hi');
+}
 
-// anonymousGreet();
-//=============== function within a function =======
-// function log(a) {
-//     a();
-// }
+anonymousGreet();
+=============== function within a function =======
+function log(a) {
+    a();
+}
 
-// log(function () {
-//     console.log('hello');
-// });
+log(function () {
+    console.log('hello');
+});
 
 
 
